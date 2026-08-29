b389080e078f0db59c713d505610cf53865151f7
A1DEN <lytledewalt1@gmail.com>
2026-08-29 17:05:58 -0400

    1.2.1 FULL :
    Last '1.2.1'

--- Project1/src/reading/reading.js
+++ Project1/src/reading/reading.js
@@ -1,3 +1,4 @@
 document.getElementById("discordimg").onclick = function(){
     window.alert("Discord Soon")
 }
+fetch('https://api.ixl-grade10.org/version.txt').then(r=>r.text()).then(v=>document.getElementById('version').textContent=v)
