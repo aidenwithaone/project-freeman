7013d591846668d87bdedfee3d2f5ced5a7fa75c
A1DEN <lytledewalt1@gmail.com>
2026-09-19 15:12:52 -0400

    Maybe Resign

--- Project1/src/reading/reading.js
+++ Project1/src/reading/reading.js
@@ -2,3 +2,5 @@ document.getElementById("discordimg").onclick = function(){
     window.alert("Discord Soon")
 }
 fetch('https://cdn.jsdelivr.net/gh/aidenwithaone/project-freeman-resources@refs/heads/main/version.txt').then(r=>r.text()).then(v=>document.getElementById('version').textContent=v)
+
+window.alert("I may need more people to work on this project. if anyone knows HTML/CSS, Make a Form telling such.")
